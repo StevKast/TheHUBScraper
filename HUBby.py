4749255905c3232c7029646b8c76c94ccef660cd
Steven Kast <kastsm@miamioh.edu>
2018-05-04 14:23:01 -0400

    Adding chrome crawler that redirects to login if not already logged in

--- HUBby.py
+++ HUBby.py
@@ -6,7 +6,8 @@
 
 #react-app > div > div:nth-child(2) > div > div > div:nth-child(1) > div:nth-child(1) > div > div:nth-child(1) > h1
 
-import requests, bs4
+import requests, bs4, getpass, time
+from selenium import webdriver
 
 res = requests.get('https://muhub.campuslabs.com/engage/organization/kode2learn')
 print(res.raise_for_status)
@@ -23,3 +24,35 @@ name = soup.select('body > div.container-fluid > div > div:nth-of-type(1) > h3')
 
 print(len(name))
 print(name)
+
+browser = webdriver.Chrome(executable_path=r'chromedriver.exe')
+time.sleep(3)
+browser.get('https://muhub.campuslabs.com/engage/organization/kode2learn')
+#Proceed to login if needed
+try:
+    elem = browser.find_element_by_css_selector('#react-app > div > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) > div > a > div > span')
+    print('Found element!')
+    time.sleep(3)
+    elem.click()
+    time.sleep(3)
+except:
+    print('Not found!')
+
+#time.sleep(3)
+
+print(browser.find_element_by_id('username'))
+
+print('Now on login page')
+try:
+    print('Login attempt')
+    #login = getpass.getuser('kastsm@miamioh.edu')
+    loginField = browser.find_element_by_css_selector('#username')
+    #loginField.send_keys('kastsm@miamioh.edu')
+    print('Found loginField')
+    #password = getpass.getpass('Password: ')
+    passField = browser.find_element_by_css_selector('#password')
+    #passField.send_keys('12345')
+    #passField.submit()
+    print('Found passField')
+except:
+    print('Login not found')
