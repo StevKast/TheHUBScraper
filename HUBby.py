9688d1166a7956878fca99dc72841cc9c1776d98
Steven <kastsm@miamioh.edu>
2018-05-04 18:33:12 -0400

    Added better way to get login auth - asks in command line.

--- HUBby.py
+++ HUBby.py
@@ -25,41 +25,56 @@ from selenium import webdriver
 # print(len(name))
 # print(name)
 
+
+login = input('Username: ')
+password = getpass.getpass('Password: ')
+
 browser = webdriver.Chrome(executable_path=r'chromedriver.exe')
-time.sleep(3)
+#time.sleep(3)
 browser.get('https://muidp.miamioh.edu/cas/login')
 
-time.sleep(3)
+#time.sleep(3)
 #print(browser.find_element_by_id('username'))
 
 print('Now on login page')
 try:
     print('Login attempt')
-    #login = getpass.getuser('username: ')
     loginField = browser.find_element_by_css_selector('#username')
-    loginField.send_keys('test')
+    loginField.send_keys(login)
     print('Found loginField')
-    #password = getpass.getpass('Password: ')
     passField = browser.find_element_by_css_selector('#password')
-    passField.send_keys('12345')
+    passField.send_keys(password)
     passField.submit()
     print('Found passField')
 except:
     print('Login not found')
 
+time.sleep(3)
 
+browser.get('https://muhub.campuslabs.com/engage/organization/kode2learn')
+#Proceed to login if needed
+try:
+    signInButton = browser.find_element_by_css_selector('#react-app > div > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) > div > a > div > span')
+    print('Signing In')
+    time.sleep(3)
+    signInButton.click()
+    time.sleep(3)
+    print('Authenticated')
+    time.sleep(5)
+    primaryContact = browser.find_element_by_css_selector('#react-app > div > div:nth-child(2) > div > div > div:nth-child(1) > div:nth-child(1) > div > div:nth-child(4) > div > div:nth-child(2) > div:nth-child(2)')
+    print(primaryContact.text)
+    elem = browser.find_element_by_css_selector('#\31 303889_231f30be-afa7-406f-babf-a50d00af8f8d')
+    print(elem.text)
+    elem.click()
+    time.wait(1.5)
+    name = browser.find_element_by_css_selector('#member_card_231f30be-afa7-406f-babf-a50d00af8f8d > div:nth-child(2) > div > div:nth-child(3)')
+    print(name.text)
+    email = browser.find_element_by_css_selector('#member_card_231f30be-afa7-406f-babf-a50d00af8f8d > div:nth-child(2) > div > div:nth-child(4) > a')
+    print(email.text)
+except:
+    print('Not found!')
 
-
-# browser.get('https://muhub.campuslabs.com/engage/organization/kode2learn')
-# #Proceed to login if needed
-# try:
-#     elem = browser.find_element_by_css_selector('#react-app > div > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) > div > a > div > span')
-#     print('Found element!')
-#     time.sleep(3)
-#     #elem.click()
-#     time.sleep(3)
-# except:
-#     print('Not found!')
+#<div id="1303889_231f30be-afa7-406f-babf-a50d00af8f8d" style="color: rgba(0, 0, 0, 0.87); background-color: rgb(255, 255, 255); transition: all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms; box-sizing: border-box; font-family: &quot;Source Sans Pro&quot;, sans-serif; -webkit-tap-highlight-color: rgba(0, 0, 0, 0); box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 6px, rgba(0, 0, 0, 0.12) 0px 1px 4px; border-radius: 2px; margin: 10px 5px; padding: 20px 10px 5px; text-align: center; min-height: 200px;"><div #style="line-height: 0; margin-bottom: 20px;"><div alt="" size="75" style="color: rgb(255, 255, 255); background-color: rgb(101, 101, 101); user-select: none; display: inline-flex; align-items: center; justify-content: center; font-size: 37.5px; border-radius: 50%; height: 75px; width: 75px;">M</div></div><div style="font-size: 14px; text-overflow: ellipsis; overflow: hidden; font-weight: bold;">PRESIDENT</div><div style="margin: 5px 0px; font-size: 17px;">Mike Wedzikowski</div></div>
 
 #time.sleep(3)
 #TODO: Prompt login then open the hub to get data
