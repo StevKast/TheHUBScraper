10858f37ab3f4ae8baa512cf563b9d7c94c07808
Steven <kastsm@miamioh.edu>
2018-06-22 10:51:39 -0400

    Adding basic UI in kivy

--- HUBby.py
+++ HUBby.py
@@ -1,71 +1,120 @@
 import requests, bs4, getpass, time
 from selenium import webdriver
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.properties import ObjectProperty
 
-login = input('Username: ')
-password = getpass.getpass('Password: ')
+class Scraper:
 
-browser = webdriver.Chrome(executable_path=r'chromedriver.exe')
-browser.get('https://muidp.miamioh.edu/cas/login')
+    def credpromt():
+        login = input('Username: ')
+        password = getpass.getpass('Password: ')
 
-#time.sleep(3)
-#print(browser.find_element_by_id('username'))
+    def scrape(promptpass):
 
-#print('Now on login page')
-try:
-    print('Login attempt')
-    loginField = browser.find_element_by_css_selector('#username')
-    loginField.send_keys(login)
-    #print('Found loginField')
-    passField = browser.find_element_by_css_selector('#password')
-    passField.send_keys(password)
-    passField.submit()
-    #print('Found passField')
-except:
-    print('Login not found')
+        if promptpass:
+            credpromt()
 
-time.sleep(1)
+        browser = webdriver.Chrome(executable_path=r'chromedriver.exe')
+        browser.get('https://muidp.miamioh.edu/cas/login')
 
-browser.get('https://muhub.campuslabs.com/engage/organizations')
-#Proceed to login if needed
+        #time.sleep(3)
+        #print(browser.find_element_by_id('username'))
 
-signInButton = browser.find_element_by_css_selector('#react-app > div > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) > div > a > div > span')
-print('Signing In')
-time.sleep(1)
-signInButton.click()
-time.sleep(1)
-print('Authenticated')
-time.sleep(1)
+        #print('Now on login page')
+        try:
+            print('Login attempt')
+            loginField = browser.find_element_by_css_selector('#username')
+            loginField.send_keys(login)
+            #print('Found loginField')
+            passField = browser.find_element_by_css_selector('#password')
+            passField.send_keys(password)
+            passField.submit()
+            #print('Found passField')
+        except:
+            print('Login not found')
 
-#Find org on page
-item = 1;
-while item < 754:
+        time.sleep(1)
 
-    time.sleep(1)
+        browser.get('https://muhub.campuslabs.com/engage/organizations')
+        #Proceed to login if needed
 
-    org = browser.find_element_by_css_selector('#org-search-results > div > div > div:nth-child(' + str(item) + ')')
-    org.click()
+        signInButton = browser.find_element_by_css_selector('#react-app > div > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) > div > a > div > span')
+        print('Signing In')
+        time.sleep(1)
+        signInButton.click()
+        time.sleep(1)
+        print('Authenticated')
+        time.sleep(1)
 
-    time.sleep(2)
+        #Find org on page
+        item = 1;
+        while item < 754:
 
-    try:
-        orgName = browser.find_element_by_css_selector('#react-app > div > div:nth-child(2) > div > div > div:nth-child(1) > div:nth-child(1) > div > div:nth-child(1) > h1')
-        print('Org Name: ' + orgName.text)
-        primaryContact = browser.find_element_by_css_selector('#react-app > div > div:nth-child(2) > div > div > div:nth-child(1) > div:nth-child(1) > div > div:nth-child(4) > div > div:nth-child(2) > div:nth-child(2)')
-        print("Primary Contact: " + primaryContact.text)
-        pass
-    except:
-        print('No Contact Info')
+            time.sleep(1)
 
-    time.sleep(2)
+            org = browser.find_element_by_css_selector('#org-search-results > div > div > div:nth-child(' + str(item) + ')')
+            org.click()
 
-    browser.back()
+            time.sleep(2)
 
-    if item % 10 == 0:
-        loadMore = browser.find_element_by_css_selector('#react-app > div > div:nth-child(2) > div > div > div:nth-child(2) > div > div:nth-child(2) > div:nth-child(2) > button > div > div > span')
-        loadMore.click()
+            try:
+                orgName = browser.find_element_by_css_selector('#react-app > div > div:nth-child(2) > div > div > div:nth-child(1) > div:nth-child(1) > div > div:nth-child(1) > h1')
+                print('Org Name: ' + orgName.text)
+                primaryContact = browser.find_element_by_css_selector('#react-app > div > div:nth-child(2) > div > div > div:nth-child(1) > div:nth-child(1) > div > div:nth-child(4) > div > div:nth-child(2) > div:nth-child(2)')
+                print("Primary Contact: " + primaryContact.text)
+                pass
+            except:
+                print('No Contact Info')
 
-    item += 1
+            time.sleep(2)
 
-#TODO: Export to CSV
-time.sleep(10)
-browser.quit()
+            browser.back()
+
+            if item % 10 == 0:
+                loadMore = browser.find_element_by_css_selector('#react-app > div > div:nth-child(2) > div > div > div:nth-child(2) > div > div:nth-child(2) > div:nth-child(2) > button > div > div > span')
+                loadMore.click()
+
+            item += 1
+
+        #TODO: Export to CSV
+        time.sleep(10)
+        browser.quit()
+#End Scraper class
+
+#########################
+#  UI
+#########################
+
+def LoginScreen():
+
+    #super(LoginScreen, self).__init__(**kwargs)
+    layout = BoxLayout(orientation='vertical', padding=('50','50'))
+    userLayout = BoxLayout(orientation='horizontal')
+    userLayout.add_widget(Label(text='User Name'))
+    username = TextInput(multiline=False)
+    userLayout.add_widget(username)
+    layout.add_widget(userLayout)
+    passLayout = BoxLayout(orientation='horizontal')
+    passLayout.add_widget(Label(text='Password'))
+    password = TextInput(password=True, multiline=False)
+    passLayout.add_widget(password)
+    layout.add_widget(passLayout)
+    return layout
+
+class LoginForm(BoxLayout):
+
+    login_cred = ObjectProperty()
+
+    def get_login_cred(self, username, password):
+        print('Unique Id: ' + username + " Password: " + password)
+
+class ScrapeApp(App):
+    pass
+
+#End MyApp class
+
+if __name__ == '__main__':
+    ScrapeApp().run()
